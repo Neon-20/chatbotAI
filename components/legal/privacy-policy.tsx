08851b0bc991e76ca8a8b8c5099510fbc5575df1
Neon-20 <rajveer23pr@gmail.com>
2025-06-17 15:12:02 +0530

    feat: t&c and pp update

--- components/legal/privacy-policy.tsx
+++ components/legal/privacy-policy.tsx
@@ -1,22 +1,12 @@
-import { Button } from "@/components/ui/button"
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
-
 export default function PrivacyPolicy() {
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <p className="cursor-pointer px-1 underline hover:animate-pulse hover:text-black">
-          Privacy Policy
-        </p>
-      </DialogTrigger>
-      <DialogContent className="w-[70vw] overflow-auto">
-        <div className="container mx-auto px-4 py-8">
-          <h1 className="mb-6 text-3xl font-bold">Privacy Policy</h1>
-          <div className="prose max-w-none">
-            <h1 className="items-center text-2xl font-semibold">TBA</h1>
-          </div>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <a
+      href="https://alterdomusgroup.sharepoint.com/sites/PPD/PPD/Forms/UserView.aspx?id=%2Fsites%2FPPD%2FPPD%2FPersonal Data Protection Process%2FGroup Process%2FG80-003 WI04 Global Employee Privacy Notice%2Edocx%2Epdf&parent=%2Fsites%2FPPD%2FPPD%2FPersonal Data Protection Process%2FGroup Process"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="cursor-pointer px-1 underline hover:animate-pulse hover:text-black"
+    >
+      Employee Privacy Policy
+    </a>
   )
 }
